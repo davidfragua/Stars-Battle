335cef75b8dfe2adac0bc6cbada9bd1a87053800
David Fragua <davidfragua@gmail.com>
2022-09-30 17:46:28 +0200

    solucionado bug que no se mostraba el canvas

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ const canvas = document.querySelector("#my-canvas");
 const ctx = canvas.getContext("2d");
 const startScreen = document.querySelector("#splash-screen");
 const startBtn = document.querySelector("#start-btn");
-const gameScreen = document.querySelector("#game-screen");
 const gameOverScreen = document.querySelector("#gameOver-screen")
 
 let gameObj;
@@ -16,7 +15,7 @@ const startGame = () =>{
     console.log("iniciando")
     // inicio el juego y oculto la splash screen
     startScreen.style.display = "none";
-    gameScreen.style.display = "block";
+    canvas.style.display = "block";
     
 
     // nueva versión del juego

c4ef5bc855ad60e63b299ecf27846674fff95086
David Fragua <davidfragua@gmail.com>
2022-10-03 10:57:08 +0200

    movimiento enemigos añadido

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -1,13 +1,14 @@
 class Enemies {
 
-    constructor() {
+    constructor(xParam) {
         // propiedades del enemigo
         this.enemy = new Image();
         this.enemy.src = "./images/enemigoAzul2.png";
-        this.x = 275;
+        this.x = xParam;
         this.y = 100;
         this.w = 50;
         this.h = 70;
+        this.speed = 3;
 
 
     }
@@ -17,4 +18,11 @@ class Enemies {
     drawEnemy = () =>{
         ctx.drawImage(this.enemy, this.x , this.y , this.w , this.h );
     } ;
+
+
+    // movimiento de los enemigos
+    moveEnemy = () => {
+        this.y = this.y + this.speed;
+    }
+
 }
--- scripts/game.js
+++ scripts/game.js
@@ -8,13 +8,11 @@ class Game {
         this.playerObj = new Player();
         this.enemyObj = new Enemies();
 
+        this.enemiesArr = [];
+        this.frames = 0;
 
 
     }
-
-
-
-
 
 
     // dibujar fondo
@@ -22,20 +20,44 @@ class Game {
         ctx.drawImage(this.fondo, 0, 0, canvas.width, canvas.height);
     }
 
+    // añadir enemigos
+    addEnemy = () =>{
+        if (this.frames % 120 === 0){
+
+            let randomNum = Math.random() * canvas.width - 50;
+            let randomXint = Math.floor(randomNum)
+
+            let newEnemy = new Enemies(randomXint)
+            this.enemiesArr.push(newEnemy);
+        }
+    }
+
 
 
 
 gameLoop = () =>{
 
+    this.frames = this.frames + 1;
+
     // 1. limpiar canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     // 2. acciones
+     // this.enemyObj.moveEnemy();
+    this.enemiesArr.forEach((eachEnemy) => {
+        eachEnemy.moveEnemy()
+      })
+
 
     // 3. dibujado
     this.drawFondo();
     this.playerObj.drawPlayer();
-    this.enemyObj.drawEnemy();
+    // this.enemyObj.drawEnemy();
+    this.enemiesArr.forEach((eachEnemy) =>{
+        eachEnemy.drawEnemy()
+    })
+   
+    this.addEnemy();
 
     // 4. control de la recursión
     requestAnimationFrame(this.gameLoop);
